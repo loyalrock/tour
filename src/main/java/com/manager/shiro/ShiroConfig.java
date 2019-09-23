d673475ca9c7bce28964df0c92784bcbdc906e5f
zhongyan <496214342@qq.com>
2019-09-24 00:19:52 +0800

    项目列表无法出现bug

--- src/main/java/com/manager/shiro/ShiroConfig.java
+++ src/main/java/com/manager/shiro/ShiroConfig.java
@@ -31,6 +31,7 @@ public class ShiroConfig {
         //拦截器.
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
         // 配置不会被拦截的链接 顺序判断
+        filterChainDefinitionMap.put("/files/**", "anon");
         filterChainDefinitionMap.put("/user/login", "anon");
         filterChainDefinitionMap.put("/project/index", "anon");
         filterChainDefinitionMap.put("/system/login", "anon");
